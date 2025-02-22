2f2e3a8fc9d8de69a77ca305064913896acac0ec
NeerajPrasanna <neerajpv2005@gmail.com>
2025-02-22 15:01:12 +0530

    commented 2nd and 3rd image for now

--- advert-react/src/App.jsx
+++ advert-react/src/App.jsx
@@ -6,14 +6,14 @@ const fadeImages = [
     url: 'https://d1csarkz8obe9u.cloudfront.net/themedlandingpages/tlp_hero_shoe-advertisement-poster-12c95286ead13a541ce7adce74bb7f06.jpg?ts%20=%201706013019',
     caption: 'First Slide'
   },
-  {
+  /*{
     url: 'https://2.imimg.com/data2/IS/TN/MY-2561564/advertisement-designing-services-500x500.jpg',
     caption: 'Second Slide'
   },
   {
     url: 'https://newspaperads.ads2publish.com/wp-content/uploads/2019/02/drools-let-the-only-thing-your-pet-bites-into-be-real-ad-delhi-times-03-02-2019.png',
     caption: 'Third Slide'
-  },
+  },*/
 ];
 
 const Slideshow = () => {
